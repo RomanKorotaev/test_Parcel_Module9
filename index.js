8f7d60bccb6bfbd6598e8ac756c914503888bcb6
Roman Korotaev <kro1986@ukr.net>
2021-08-26 16:56:32 +0300

    Протестирован экспорт пространства имён

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 
 import data from "./defaultexport";
 
-import {functionOne,functionTwo, functionThree, x, y  } from "./namedexport";
-
 
 console.log(" Это файл index.js")
 console.log("data from ./defaultexport =  ", data)
+
+
+import {functionOne,functionTwo, functionThree, x as value, y  } from "./namedexport";
 
 functionOne();
 functionTwo();
 functionThree();
 
-console.log(`x  from ./namedexport = ${x}`)
-console.log(`y  from ./namedexport = ${y}` )
+//console.log(`x  from ./namedexport = ${x}`)
+console.log(`x has name in our local file  value  from ./namedexport = ${value}`)
+console.log(`y  from ./namedexport = ${y}`)
 
+
+import * as apiService from "./namespace_export"
+
+apiService.function4();
+
+console.log(`apiService.function5 = ${apiService.function5}`)
+
+console.log('apiService.t = ', apiService.t);
+
+console.log('apiService.d = ',apiService.d )
 
 
 
---
+++ namespace_export.js
@@ -0,0 +1,13 @@
+export function function4() {
+    console.log('This is function4()  from ./namespace_export')
+};
+
+export const function5 = () => { console.log('This is function5   from ./namespace_export') };
+
+export const function6 = () => { console.log('This is function6   from ./namespace_export') };
+
+export const t = 10;
+
+export const d = 'dsfrsrhsfhsf from namespace_export'
+
+export default 777 ;
